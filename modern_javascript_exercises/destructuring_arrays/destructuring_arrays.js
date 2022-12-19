60fd1dd4c9e0fe415ae88a0036357ffe8f972630
Peter Viegas <peterviegas@outlook.com>
2022-12-19 11:15:24 +0000

    Change destructuring_arrays - used basic variable assignment

--- modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
+++ modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
@@ -5,6 +5,9 @@ export function sequence(order) {
     //const second = order[1];
     //const third = order[2];
 
-    //return first + ", " + second + ", " + third;
-    return order.toString().replace(/,/g, ", ");
+    const [first, second, third] = order; // this pulls out [0], [1] and [2] into variables we are naming `first` `second` and `third`
+
+    return first + ", " + second + ", " + third;
+
+    //return order.toString().replace(/,/g, ", "); Essa tinha sido a minha solução, mas precisa utilizar a destructuring
 }
