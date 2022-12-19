a8c2fe3196fad29cce6517de19f64eba6a09adf4
Peter Viegas <peterviegas@outlook.com>
2022-12-19 10:58:48 +0000

    Change default_parameters -  set a default parameter on person

--- modern_javascript_exercises/default_parameters/default_parameters.js
+++ modern_javascript_exercises/default_parameters/default_parameters.js
@@ -1,10 +1,23 @@
 // Instructions can be found in default_parameters.md
 
-export const sayHello = (person) => {
+//Forma passada pelo Neil mais simples e comum de implementar
+//export const sayHello = (person = "you") => { 
+export const sayHello = (person = "you") => {
+    //return `Hello, ${person === undefined ? "you!" : person + "!"}`;
+    return greet(person);
+};
+
+function greet(name, message = `Hello, ${name}!`) {
+    return message;
+}
+
+
+//Minha implementação está ok, mas não comum
+/*export const sayHello = (person) => {
     //return `Hello, ${person === undefined ? "you!" : person + "!"}`;
     return greet(person);
 };
 
 function greet(name = `you`, message = `Hello, ${name}!`) {
     return message;
-}
+}*/
